b8b0d83237edef6ca50ba7fb34407a722a048c1a
Laurent Pages <laurent.pages@elca.ch>
2020-12-11 16:55:48 +0100

    CLOUDTRUST-2905: add flag for using (or not) the artifact binding for logout process (assuming the login was done using artifact binding)

--- themes/src/main/resources/theme/base/admin/resources/js/controllers/clients.js
+++ themes/src/main/resources/theme/base/admin/resources/js/controllers/clients.js
@@ -1098,6 +1098,7 @@ module.controller('ClientDetailCtrl', function($scope, realm, client, flows, $ro
     $scope.samlOneTimeUseCondition = false;
     $scope.samlMultiValuedRoles = false;
     $scope.samlArtifactBinding = false;
+    $scope.samlArtifactBindingLogout = false;
     $scope.samlServerSignature = false;
     $scope.samlServerSignatureEnableKeyInfoExtension = false;
     $scope.samlAssertionSignature = false;
@@ -1183,6 +1184,14 @@ module.controller('ClientDetailCtrl', function($scope, realm, client, flows, $ro
                 $scope.samlArtifactBinding = true;
             } else {
                 $scope.samlArtifactBinding = false;
+            }
+        }
+
+        if ($scope.client.attributes["saml.artifact.binding.logout"]) {
+            if ($scope.client.attributes["saml.artifact.binding.logout"] == "true") {
+                $scope.samlArtifactBindingLogout = true;
+            } else {
+                $scope.samlArtifactBindingLogout = false;
             }
         }
 
@@ -1615,6 +1624,12 @@ module.controller('ClientDetailCtrl', function($scope, realm, client, flows, $ro
         } else {
             $scope.clientEdit.attributes["saml.artifact.binding"] = "false";
         }
+        if ($scope.samlArtifactBindingLogout == true) {
+            $scope.clientEdit.attributes["saml.artifact.binding.logout"] = "true";
+        } else {
+            $scope.clientEdit.attributes["saml.artifact.binding.logout"] = "false";
+        }
+
         if ($scope.samlServerSignature == true) {
             $scope.clientEdit.attributes["saml.server.signature"] = "true";
         } else {
